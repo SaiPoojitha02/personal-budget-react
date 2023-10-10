227dec4d4716a9aee1f1372ac819b4a004ed19e1
SaiPoojitha02 <143846108+SaiPoojitha02@users.noreply.github.com>
2023-10-09 21:20:23 -0400

    Added Backend files

---
+++ server.js
@@ -0,0 +1,23 @@
+// Budget API
+
+const fs = require('fs');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3030;
+
+app.use(cors());
+
+
+
+
+const json = fs.readFileSync('Categories.json', 'utf8');
+const Data = JSON.parse(json);
+app.get('/budget', (req, res) => {
+    res.json(Data);
+});
+
+
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
+});
